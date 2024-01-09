0ed568bb342126cfc017b8310b48cbb2760d6f46
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-01-09 19:25:16 +0530

    Add styles for root and body elements

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Leva } from "leva";
-import { MantineProvider } from "@mantine/core";
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={{
-                globalStyles: (_theme) => ({
-                body: {
-                    width: "100vw",
-                    height: "100vh",
-                },
-                "#root": {
-                    width: "100%",
-                    height: "100%",
-                },
-                }),
-            }}
-        >
-            <App />
-        </MantineProvider>
-        
+        <App />
     </React.StrictMode>
 );

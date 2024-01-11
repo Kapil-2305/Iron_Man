20e59e9f6776df6c02ae59dd0e063c28d8a35d9b
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-01-11 23:29:01 +0530

    Add Leva controls and animations to Ironman component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./components/Experience";
+import {useControls} from "leva";
 
 const App = () => {
+    const {color} = useControls({color: "#171720"});
+    const {enable} = useControls({enable: {value: true, label: "Enable Orbit Controls"}});
+    const {autoRotate} = useControls({autoRotate: {value: false, label: "Auto Rotate"}});
+    
     return (
         <>
-            <Canvas camera={{ position: [1, 1.5, 2.5], fov: 50 }} shadows>
+            <Canvas camera={{ position: [0, 1.8, 5], fov: 50 }} shadows>
+                <color attach="background" args={[color]} />
                 <Experience />
             </Canvas>
         </>

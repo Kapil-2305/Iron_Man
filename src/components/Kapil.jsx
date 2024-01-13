e579cffee3179abf321c7cabf181c7b4dfd5dabe
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-01-13 08:37:46 +0530

    remove stop function to make animation smoother

--- src/components/Kapil.jsx
+++ src/components/Kapil.jsx
@@ -25,7 +25,7 @@ const Kapil = (props) => {
     useEffect(()=>{
         actions[animation].reset().fadeIn(0.5).play();
         return ()=>{
-            actions[animation].reset().fadeOut(0.5).stop();
+            actions[animation].reset().fadeOut(0.5);
         }
     }, [animation]);
 

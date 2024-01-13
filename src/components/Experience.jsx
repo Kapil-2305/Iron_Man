464ce2f9f1729eb57309b4c35ac1ce898c50abc8
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-01-13 15:57:14 +0530

    Add gsap library and Background component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,11 @@
+import Background from "./Background";
 import Kapil from "./Kapil"
 
 
 const Experience = () => {
     return (
         <>
+            <Background />
             <ambientLight intensity={2.8}/>
             <directionalLight position={[-5, 5, 5]} castShadow shadow-mapSize={1024} />
             <group position={[0, -1, 0]}>
